11d7bcc47bb70720cc91ee7700e4dc5b2e000851
Vladymyr Tsybuzghin <periodical7rio@gmail.com>
2022-04-28 12:28:51 +0300

    init

--- test_task/users_list/js/script.js
+++ test_task/users_list/js/script.js
@@ -9,17 +9,41 @@ toggler.addEventListener("click", () => {
     }
 });
 
-$(function () {
-    $(document).ready(getUsers());
-    function getUsers() {
-        $.ajax({
-            type: "GET", // если в доке POST, замени на него
-            dataType: "json",
-            contentType: "application/json; charset=utf-8",
-            url: 'https://zentesting.herokuapp.com/show_users/', //свою ссылку поставишь
-            success: function (data) {
-                console.log(data);
-            }
-        });
-    }
+//Блок удаления пользователя
+
+const del_but = document.querySelector(".dropdown-item");
+const user = document.querySelector(".row-2");
+del_but.addEventListener("click", () => {
+    user.classList.add("hide");
+});
+
+const del_but_2 = document.querySelector(".test_dropdown-item");
+const user_2 = document.querySelector(".row-3");
+del_but_2.addEventListener("click", () => {
+    user_2.classList.add("hide");
+});
+
+//Блок запроса в базу пользователей
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    };
+    rawFile.send(null);
+}
+
+// calling above function (+вывод данных в меню)
+readTextFile('../js/response_1651130785854.json', function(text){
+    var data = JSON.parse(text); //parse JSON
+    console.log(data[0]);
+    document.querySelector('.test_id').textContent = data[0].id;
+    document.querySelector('.test_name').textContent = data[0].user_name;
+    document.querySelector('.test_email').textContent = data[0].email;
+    document.querySelector('.test_password').textContent = data[0].password;
+    document.querySelector('.test_status').textContent = data[0].status;
 });
